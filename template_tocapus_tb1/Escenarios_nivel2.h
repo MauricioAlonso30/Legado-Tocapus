1ddc10fd114b45cc7ffc09a9d61a09d8af051a4a
Darrel <darreldeckeruiz@gmail.com>
2026-05-09 17:32:02 -0500

    cambios

---
+++ template_tocapus_tb1/Escenarios_nivel2.h
@@ -0,0 +1,16 @@
+#include "Recursos.h"
+
+
+extern int matriz_nivel2[ALTO][ANCHO];
+
+extern int matriz_mensaje_nivel2[ALTO][ANCHO];
+
+extern int matriz_escena1_nivel2[ALTO][ANCHO];
+
+extern int matriz_escena2_nivel2[ALTO][ANCHO];
+
+extern int matriz_escena3_nivel2[ALTO][ANCHO];
+
+extern int matriz_escena_juego_nivel2[ALTO][ANCHO];
+
+extern int matriz_escena4_nivel2[ALTO][ANCHO];
---
+++ template_tocapus_tb1/Escenarios_nivel3.h
@@ -0,0 +1,16 @@
+#include "Recursos.h"
+
+
+extern int matriz_nivel3[ALTO][ANCHO];
+
+extern int matriz_mensaje_nivel3[ALTO][ANCHO];
+
+extern int matriz_escena1_nivel3[ALTO][ANCHO];
+
+extern int matriz_escena2_nivel3[ALTO][ANCHO];
+
+extern int matriz_escena3_nivel3[ALTO][ANCHO];
+
+extern int matriz_escena_juego_nivel3[ALTO][ANCHO];
+
+extern int matriz_escena4_nivel3[ALTO][ANCHO];
